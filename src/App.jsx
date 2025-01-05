063011cb57433ecec63df4340dbf30938d9d0f75
Fais <pallakangoding@.com>
2025-01-06 02:58:11 +0800

    add Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Hero from "./component/Hero";
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Test</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+
+      <section className="z-0 min-h-screen bg-blue-500"></section>
     </main>
   );
 };
