327c743b4463948b8dcf2e7e462079eb68b6a7f8
Fais <pallakangoding@.com>
2025-01-11 00:23:29 +0800

    add Contact, Footer, Story Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Hero from "./component/Hero";
 import About from "./component/About";
 import Navbar from "./component/Navbar";
 import Features from "./component/Features";
+import Story from "./component/Story";
+import Contact from "./component/Contact";
+import Footer from "./component/Footer";
 
 const App = () => {
   return (
@@ -11,6 +14,9 @@ const App = () => {
       <Hero />
       <About />
       <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   );
 };
